e9478c5cdb1d66dbe3f2ca5a613d94fe34fce72f
Ray S <rayspace@protonmail.com>
2021-08-02 09:37:59 -0600

    deployment changes

--- Sites/src/main/java/com/skilldistillery/sites/SitesApplication.java
+++ Sites/src/main/java/com/skilldistillery/sites/SitesApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.sites;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SitesApplication {
+public class SitesApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SitesApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SitesApplication.class, args);
